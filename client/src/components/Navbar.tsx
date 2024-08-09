128854aaf263cadc4244c2c78b6c55da755aee67
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-08-10 01:49:13 +0530

    Add Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,13 +3,14 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
+import { useStateContext } from '../context';
 import { CustomButton } from './';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const address = "0xSomething";
+ const { connect, address } = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
