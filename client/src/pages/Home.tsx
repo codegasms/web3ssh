29fc4b19823cd1837051b53a2b58e308204ac366
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-08-10 01:48:12 +0530

    Add Homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+
+import { DisplayCampaigns } from '../components';
+import { useStateContext } from '../context';
 
 export default function Home() {
+
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-white">Home</h1>
-    </div>
-  );
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  )
 }
