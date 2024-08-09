76c677a1ae39aea752dab9277f6ad2e8f492da3a
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-08-10 01:49:42 +0530

    Finalise Main.tsx (clientID & Chain)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,21 @@
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThirdwebProvider } from "thirdweb/react";
+
 import { App } from "./App";
+import { StateContextProvider } from './context';
 import "./index.css";
+
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
+    <ThirdwebProvider activeChain={Sepolia} clientId="ac95123ad2a75a0a66f6e6ae83691f54">
       <Router>
-      <App />
+        <StateContextProvider>
+          <App />
+        </StateContextProvider>
       </Router>
     </ThirdwebProvider>
   </React.StrictMode>
